0b6577a86f4c3444eb5362550ad6e38baff27d76
zs2love <zs2love@126.com>
2017-06-29 07:07:34 +0800

    Adding the security module.

---
+++ src/main/java/com/example/readinglist/ReaderRepository.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package com.example.readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author shuai.b.zhang
+ *
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+
+}
