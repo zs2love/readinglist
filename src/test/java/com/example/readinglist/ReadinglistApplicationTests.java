ad5a3557b1cce3978d6ac980870e0447434b95fc
zs2love <zs2love@126.com>
2017-06-30 00:15:44 +0800

    Add the security and some of the property settings.

---
+++ src/test/java/com/example/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.readinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=ReadinglistApplication.class)
+@WebAppConfiguration
+public class ReadinglistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
