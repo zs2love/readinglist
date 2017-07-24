2c4628d7e203a89529ccf0136d28bff34b4e1593
zs2love <zs2love@126.com>
2017-07-24 23:31:56 +0800

    Commit for security update.

--- src/main/java/com/example/readinglist/ReadingListController.java
+++ src/main/java/com/example/readinglist/ReadingListController.java
@@ -9,33 +9,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/readingList")
+@RequestMapping("/")
 public class ReadingListController {
 
-  private static final String reader = "craig";
-  
+	private static final String reader = "craig";
+
+	
+	private AmazonProperties amazonProps;
+	
 	private ReadingListRepository readingListRepository;
 
+	
+	
 	@Autowired
-	public ReadingListController(ReadingListRepository readingListRepository) {
+	public ReadingListController(AmazonProperties amazonProps, ReadingListRepository readingListRepository) {
+		this.amazonProps = amazonProps;
 		this.readingListRepository = readingListRepository;
 	}
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+
+	@RequestMapping(method = RequestMethod.GET)
 	public String readersBooks(Model model) {
-		
+
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
+			model.addAttribute("reader", reader);
+			model.addAttribute("amazonId", amazonProps.getAssociatedId());
 		}
 		return "readingList";
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
+
+	@RequestMapping(method = RequestMethod.POST)
 	public String addToReadingList(Book book) {
 		book.setReader(reader);
 		readingListRepository.save(book);
-		return "redirect:/readingList";
+		return "redirect:/";
 	}
-	
+
+
 }
--- src/main/java/com/example/readinglist/SecurityConfig.java
+++ src/main/java/com/example/readinglist/SecurityConfig.java
@@ -3,8 +3,11 @@
  */
 package com.example.readinglist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,28 +20,29 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  * @author shuai.b.zhang
  *
  */
+
+@Profile("development")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 	@Autowired
 	private ReaderRepository readerRepository;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").access("hasRole('READER')")
-				.antMatchers("/**").permitAll().and().formLogin()
-				.loginPage("/login").failureUrl("/login?error=true");
+		http.authorizeRequests().antMatchers("/").access("hasRole('READER')").antMatchers("/**").permitAll().and()
+				.formLogin().loginPage("/login").failureUrl("/login?error=true");
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(new UserDetailsService() {
 
 			@Override
-			public UserDetails loadUserByUsername(String username)
-					throws UsernameNotFoundException {
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				logger.info("User Name for login is: {0}", username);
 				return readerRepository.findOne(username);
 			}
 		});
